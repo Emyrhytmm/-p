6380317fae89790960d5d11d31a231e8ed84c347
Emyrhytmm <houkoucelik07@gmail.com>
2022-11-29 20:59:51 +0300

    Add project files.

---
+++ Gizli(Mail Gönderme)/Program.cs
@@ -0,0 +1,11 @@
+﻿
+using Utility;
+
+
+   MailSender.DefineSenderInformation("pisitest@outlook.com", "pisipisi07");
+
+   MailSender.Sendmail("onurcelik0739@outlook.com", "Hayat", "çok zor hocam -emir:)");
+
+   Console.WriteLine("Mail Gönderildi");
+
+
---
+++ Utility/MailSender.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utility
+{
+    public static class MailSender
+    {
+        private static string sendermail;
+        private static string senderpassword;
+
+        public static void DefineSenderInformation(string email, string password)
+        {
+            sendermail = email;
+            senderpassword = password;
+        }
+
+
+        public static void Sendmail(string recieverMail, string subject, string messageBody)
+        {
+            try
+            {
+                MailMessage message = new MailMessage();
+                SmtpClient smtp = new SmtpClient();
+                message.From = new MailAddress(sendermail);
+                message.To.Add(new MailAddress(recieverMail));
+                message.Subject = subject;
+                message.IsBodyHtml = true; //to make message body as html  
+                message.Body = messageBody;
+                smtp.Port = 587;
+                smtp.Host = "smtp.office365.com"; //for gmail host  
+                smtp.EnableSsl = true;
+                smtp.UseDefaultCredentials = false;
+                smtp.Credentials = new NetworkCredential(sendermail, senderpassword);
+                smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+                smtp.Send(message);
+            }
+            catch (Exception ex) {
+                throw new Exception(ex.Message);
+                
+            }
+        }
+    }
+}
